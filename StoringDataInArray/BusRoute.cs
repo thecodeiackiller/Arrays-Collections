962e4928cb1b5fe0299f84a7bda44c221a1caff9
knunn553 <knunn553@outlook.com>
2024-07-24 16:47:05 -0400

    Finished -Storing Data in Arrays- and -Finding Data in Arrays- video series on PluralSight. Most on which was review, but did learn more in detial about the System.Array class in .NET and the different properties and methods it provides including Find, FindAll, Length, and Exists. Many of these methods can take the place of for and foreach loops to save us time.

---
+++ StoringDataInArray/BusRoute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoringDataInArray
+{
+    public class BusRoute
+    {
+        public int Number { get; }
+        public string Origin => PlacesServed[0];
+        public string Destination => PlacesServed[PlacesServed.Length - 1];
+        public string[] PlacesServed { get; }
+
+        public BusRoute(int number, string[] placesServed)
+        {
+            this.Number = number;
+            this.PlacesServed = placesServed;
+        }
+
+        public bool Serves(string destination)
+        {
+            return Array.Exists(PlacesServed, places => places == Destination);
+            // Rather than returning the value it finds, Exists returns only whether or not it could find the value in the array
+            //foreach (string place in PlacesServed)
+            //{
+            //    if(place == destination)
+            //    {
+            //        return true;
+            //    }
+            //}
+            //return false;
+        }
+        public override string ToString() => $"{Number}: {Origin} -> {Destination}";
+    }
+}
---
+++ StoringDataInArray/BusRouteRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoringDataInArray
+{
+    public class BusRouteRepository
+    {
+        public static BusRoute[] InitializeRoutes()
+        {
+            return new BusRoute[] {
+                new BusRoute(40, new string[] {
+                    "Morecambe", "Lancaster", "Garstang", "Preston" }),
+                new BusRoute(42, new string[] { "Lancaster", "Garstang", "Blackpool" }),
+                new BusRoute(100, new string[] { "University", "Lancaster", "Morecambe" }),
+                new BusRoute(555, new string[] {
+                    "Lancaster", "Carnforth", "Kendal", "Windermere", "Keswick" }),
+                new BusRoute(1_000, new string[] { "Test origin", "Test destination" })
+            };
+
+        }
+    }
+}
