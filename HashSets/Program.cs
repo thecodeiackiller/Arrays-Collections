a37e5853520ae6b5601bb95d71f7455de4cb9b5f
knunn553 <knunn553@outlook.com>
2024-07-24 22:43:02 -0400

    Looked at the mighty HashSet, which is used for combining or comparing the values in two different sets of collections. HashSets are great for comparing and checking for duplicates. We can use many methods to work between two collections with HashSet out of the box methods UnionWith, IntersectWith, and ExceptWith.

---
+++ HashSets/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HashSets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // HashSets allow us to solve problems that force us to combine collections together.
+            // HashSet<T> is also great for checking for duplicate items
+            // HashSet can be thought of a 1 whole collection instead of a collection made up of individual parts
+
+            // Need to create your own scenarios where you use HashSets instead of just watching a video. It really only makes sense with something you've created step by step.
+            // Because I could care less about Bus Routs in the United Kingdom...
+
+            // IntersectWith method
+            // UnionWith
+            // ExceptWith .. and many more
+        }
+    }
+}
