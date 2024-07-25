c867ce393e73ebdc56085a13797ae57098b9a3cd
knunn553 <knunn553@outlook.com>
2024-07-24 22:12:10 -0400

    Didn't do too much following along with code as I've worked with Dictionaries quite a bit, but did learn quite a bit of stuff. Dictionaries can be thought of as storing objects in a bag. There are not sorted unless we use either SortedList or SortedDictionary. We can also used handy methods such as TryGetValue and ContainsKey. There are many other methods that I've used with Dictionaries but this was a great review.

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrays and Lists are great for index based lookups
+            // Dictionaries are great for finding items
+            // Arrays can be thought of as a list of objects
+            // Dictionaries can be thought of as like a "bag" of objects
+            // Using TryGetValue over ContainsKey will probably have a performance benefit because it only looks up the key once
+
+            // If were enumerating through the dictionary, its almost always because we want to see the values within the collection
+            // There is a SortedDictionary, but our lives will typically be much easier if we use an ordinary Dictionary.
+            // There is also a SortedList, which is basically the same as SortedDictionary. Or, they return the same result.
+            // SortedList has, for specific scenarios, a slight performance benefit over SortedDictionary 
+            // SortedList, generally speaking, is more efficient is we have already sorted our items before instantiating the SortedList
+        }
+    }
+}
